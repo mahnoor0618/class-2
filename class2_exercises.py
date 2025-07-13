84efa2521d81bf7ab9fc22c93b63e573ce388a3a
mahnoor0618 <mahnoorshehzad823@gmail.com>
2025-07-13 16:12:46 +0500

    Add files via upload

---
+++ class2_exercises.py
@@ -0,0 +1,18 @@
+#Exercise 1
+
+#Write a program that adds the digits in a 2 digit number. e.g. if the input was 35, then the output should be 3 + 5 = 8
+
+num=str(input("Enter any two digit number: "))
+
+print(f"{int(num[0])} + {int(num[1])} = {int(num[0])+int(num[1])}")
+
+#Exercise 2
+
+"""Create a program using maths and f-Strings that tells us how many days, weeks, months we have left if we live until 90 years old.
+
+It will take your current age as the input and output a message with our time left in this format:
+
+You have x days, y weeks, and z months left.
+
+Where x, y and z are replaced with the actual calculated numbers."""
+
